6efdcd2f5e0a5c9c0c51be6b719d0c0bd868f437
EnzoBarreto8893 <enzo.barreto@correo.ucu.edu.uy>
2024-04-04 00:18:53 -0300

    subiendo las carpetas a la repo

---
+++ creador de proyecto 2.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+
+set /p nombreProyecto=Ingrese el nombre del proyecto:
+mkdir %nombreProyecto%
+cd %nombreProyecto%
+dotnet new sln
+mkdir src
+mkdir docs
+mkdir test
+cd src
+dotnet new console -n Program --use-program-main
+dotnet new classlib -n Library
+cd Program
+dotnet add Program.csproj reference ..\Library\Library.csproj
+cd ..\..
+cd test
+dotnet new nunit -n Library.Tests
+cd Library.Tests
+dotnet add Library.Tests.csproj reference ..\..\src\Library\Library.csproj
+cd ..\..
+dotnet sln add src\Library\Library.csproj
+dotnet sln add src\Program\Program.csproj
+dotnet sln add test\Library.Tests\Library.Tests.csproj
+dotnet new gitignore
+code .
+
+endlocal
